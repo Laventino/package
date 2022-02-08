72aeaa57d3bc6a231e12e99103ecdc8f211cd3eb
menglong_sdx <sdx.app.2@gmail.com>
2022-02-08 08:37:32 +0700

    first commit

---
+++ package/menglong/src/MenglongServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+namespace Menglong\Package;
+use Illuminate\Support\ServiceProvider;
+class MenglongServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+    public function register()
+    {
+        
+    }
+}
